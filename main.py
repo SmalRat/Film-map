21f8d00cd808b4119b8e976ec8412ffee8aa9bcd
Shchur Oleksandr <Olexander.Shchur@gmail.com>
2022-02-11 04:10:34 +0200

    Added third layer creation, some descriptions about function work. More cache

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+"""
+Module, which reads data from a file with a films list, determines films,
+made in the given year, and geolocation of their production places.
+Then finds 10 or fewer such nearest to the given point places, makes markers
+for them, and creates a map with a  layer of that markers.
+Also, there is another layer, which contains markers
+of film shooting places in Ukraine.
+"""
+
 import pandas as pd, argparse
 import folium
 import random
@@ -7,8 +16,7 @@ from geopy.geocoders import Nominatim
 from geopy import distance
 from geopy.exc import GeocoderUnavailable
 
-
-count = {} #reminder
+count = {}  # reminder
 getLogger().setLevel(DEBUG)
 
 
@@ -33,59 +41,67 @@ def memoize_and_write(func, places_dict):
     """
     Decorator for memoization
     """
+
     def wrapper(place):
+        """
+        Wrapper. Organises the cache usage and its creation.
+        """
         try:
             place = str(place)
-
             original_place = place
+
             while True:
                 if place not in places_dict.keys() and place != "nan":
                     coordinates = func(place.replace("*", ""))
+
                     if coordinates:
                         coordinates = tuple([coordinates.point[0], coordinates.point[1]])
                         places_dict[original_place] = coordinates
                         with open("data/places_database", mode="a") as file:
                             file.write(original_place + "," + str(coordinates) + "\n")
-                        debug(str(coordinates) + " returned")  # reminder
+                        # debug(str(coordinates) + " returned")
                         return coordinates
                     else:
                         if len(place.split("*")) > 1:
-                            debug(place + "just have been cropped")
+                            # debug(place + "just have been cropped")
                             place = crop_address(place)
                         else:
                             places_dict[original_place] = coordinates
                             with open("data/places_database", mode="a") as file:
                                 file.write(original_place + "," + str(coordinates) + "\n")
-                            print(str(coordinates) + " returned for " + original_place)  # reminder
+                            print(str(coordinates) + " returned for " + original_place)
                             return coordinates
                 elif place == "nan":
-                    debug("none returned")#reminder
+                    # debug("none returned")
                     return None
                 else:
                     raw_coordinates = places_dict[place]
                     if isinstance(raw_coordinates, tuple):
-                        debug(str(raw_coordinates) + " returned")  # reminder
+                        # debug(str(raw_coordinates) + " returned")
                         return raw_coordinates
                     try:
                         try:
-                            coordinates = tuple([float(i) for i in tuple(raw_coordinates[1 : len(raw_coordinates) - 1].split(", "))])
-                            debug(str(coordinates) + " returned")  # reminder
+                            coordinates = tuple(
+                                [float(i) for i in tuple(raw_coordinates[1: len(raw_coordinates) - 1].split(", "))])
+                            # debug(str(coordinates) + " returned")
                             return coordinates
                         except ValueError:
-                            print(raw_coordinates + " caused ValueError")  # reminder
+                            print(raw_coordinates + " caused ValueError")
                             return None
                     except:
-                        print(raw_coordinates + " caused other error")  # reminder
+                        print(str(raw_coordinates) + " caused other error")
                         return None
         except GeocoderUnavailable:
             print("GeocoderUnavailable error for this location:" + place + ". Proceeding...")
             return None
+
     return wrapper
 
 
 def parsing(lst=None):
     """
     Function for parsing command line
+    #reminder
 
     >>> parsing(["2020", "80", "90", "dataset"])
     (2020, 80.0, 90.0, 'dataset')
@@ -95,6 +111,7 @@ def parsing(lst=None):
     parser.add_argument("latitude", metavar="Latitude", type=float, help="Latitude of your point.")
     parser.add_argument("longitude", metavar="Longitude", type=float, help="Longitude of your point.")
     parser.add_argument("path", metavar="Path", help="Path to your dataset.")
+
     if lst:
         results = parser.parse_args(lst)
     else:
@@ -106,23 +123,27 @@ def parsing(lst=None):
     if not -90 <= results.longitude <= 90:
         message = "%r not in range [-90, 90]" % (results.longitude,)
         raise argparse.ArgumentTypeError(message + universal_message)
+
     return results.year, results.latitude, results.longitude, results.path
 
 
 def read_csv(path):
     """
-    Reads csv file into the pandas dataframe
+    Reads csv file (path) into the pandas dataframe.
+    Also tries to open data/places_database cache file
+    and store info from that file into the dictionary.
     """
 
     data_base = pd.read_csv(path, dtype={'year': int})
     places_dict = {}
+
     try:
         with open("data/places_database") as file:
             for line in file:
                 line = line.strip("\n")
                 coma_pos = line.find(",")
                 if coma_pos != -1:
-                    places_dict[line[:coma_pos]] = line[coma_pos+1:]
+                    places_dict[line[:coma_pos]] = line[coma_pos + 1:]
     except FileNotFoundError:
         pass
 
@@ -140,13 +161,13 @@ def geolocation(data_base, year, latitude, longitude, places_dict):
     """
 
     geolocator = Nominatim(user_agent="Films map")
-    geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1, max_retries=2, swallow_exceptions=True,\
+    geocode = RateLimiter(geolocator.geocode, min_delay_seconds=0.3, max_retries=2, swallow_exceptions=True, \
                           return_value_on_exception=None, error_wait_seconds=2)
     geocode_modified = memoize_and_write(geocode, places_dict)
 
     condition = (data_base['year'] == year)
     valid_films = data_base[condition]
-    print(valid_films)
+    print(valid_films) #reminder
     location_series = valid_films['place'].apply(geocode_modified)
     valid_films["points"] = location_series
     valid_films["distance_to_the_current_point"] = 0
@@ -163,76 +184,136 @@ def geolocation(data_base, year, latitude, longitude, places_dict):
     return valid_films
 
 
-def creating_map(data_base, latitude, longitude):
+def creating_map(data_base, latitude, longitude, full_data_base):
     """
-    Creates html file containing map with nearest films.
+    Creates html file containing map with nearest films as second layer
+    and films with scenes made in Ukraine as the third.
     """
+
     def nearest_points(db):
+        """
+        Extracts from pandas DataFrame a part with the nearest 10 or fewer films to a given point.
+        """
         new_db = pd.DataFrame([])
         for i in range(min(len(db), 10)):
             idx = db["distance_to_the_current_point"].idxmin()
             new_db[i] = db.loc[idx, :]
-            if i > 0 and new_db[i]["distance_to_the_current_point"]  == new_db[i-1]["distance_to_the_current_point"]:
-                new_db[i]["points"] = (new_db[i]["points"][0] + random.random()/100, new_db[i]["points"][1] +\
-                                       random.random()/100)
+            if i > 0 and new_db[i]["distance_to_the_current_point"] == new_db[i - 1]["distance_to_the_current_point"]:
+                new_db[i]["points"] = (new_db[i]["points"][0] + random.random() / 100, new_db[i]["points"][1] + \
+                                       random.random() / 100)
             db.drop(idx)
         new_db = new_db.T
+
         return new_db
-    check = False
-    try:
-        map = folium.Map(location=[latitude, longitude],
-                     zoom_start=3)
-        check = True
-    except:
-        print("Wrong coordinates!")
-    if check:
+
+    def nearest_films(db):
+        """
+        Creates feature group containing markers with nearest films.
+        """
         fg = folium.FeatureGroup(name="Nearest films")
-        data_base = nearest_points(data_base)
-        if not data_base.empty:
-            for i in range(len(data_base["points"])):
-                if data_base.iloc[i, 2] == "NO DATA":
+        db = nearest_points(db)
+        if not db.empty:
+            for i in range(len(db["points"])):
+                if db.iloc[i, 2] == "NO DATA":
                     html = """<body>
-                            <h4>{}</h4>
-                            Year: {}<br>
-                            <a href="https://www.google.com/search?q=%22{}%22"target="_blank">{}</a>
-                            </body>
-                            """
-                    html_format = html.format(data_base.iloc[i, 0], data_base.iloc[i, 1], data_base.iloc[i, 0],\
-                                              data_base.iloc[i, 0])
+                                    <h4>{}</h4>
+                                    Year: {}<br>
+                                    <a href="https://www.google.com/search?q=%22{}%22"target="_blank">{}</a>
+                                    </body>
+                                    """
+                    html_format = html.format(db.iloc[i, 0], db.iloc[i, 1], db.iloc[i, 0], \
+                                              db.iloc[i, 0])
                 else:
                     html = """<body>
-                            <h4>{}</h4>
-                            Year: {}<br>
-                            Additional info: {}
-                            <a href="https://www.google.com/search?q=%22{}%22"target="_blank">{}</a>
-                            </body>
-                            """
-                    html_format = html.format(data_base.iloc[i, 0], data_base.iloc[i, 1], data_base.iloc[i, 2],\
-                                              data_base.iloc[i, 0], data_base.iloc[i, 0])
+                                    <h4>{}</h4>
+                                    Year: {}<br>
+                                    Additional info: {}
+                                    <a href="https://www.google.com/search?q=%22{}%22"target="_blank">{}</a>
+                                    </body>
+                                    """
+                    html_format = html.format(db.iloc[i, 0], db.iloc[i, 1], db.iloc[i, 2], \
+                                              db.iloc[i, 0], db.iloc[i, 0])
 
                 iframe = folium.IFrame(html=html_format,
                                        width=200,
                                        height=100)
-                point = data_base.iloc[i, 4]
+                point = db.iloc[i, 4]
                 if point:
                     lat = point[0]
                     lon = point[1]
                     fg.add_child(folium.Marker(location=[lat, lon],
                                                popup=folium.Popup(iframe),
                                                icon=folium.Icon(color="red")))
+        return fg
+
+    def ukr_films(db):
+        """
+        Creates feature group containing markers with Ukrainian films.
+        """
+        ukr_films_db = db["Ukraine" in db['place']]
+        ukr_fg = folium.FeatureGroup(name="Ukrainian films")
+        print(ukr_films_db)
+
+        if not ukr_films_db.empty:
+            for i in range(len(ukr_films_db["points"])):
+                if ukr_films_db.iloc[i, 2] == "NO DATA":
+                    html = """<body>
+                                    <h4>{}</h4>
+                                    Year: {}<br>
+                                    <a href="https://www.google.com/search?q=%22{}%22"target="_blank">{}</a>
+                                    </body>
+                                    """
+                    html_format = html.format(ukr_films_db.iloc[i, 0], ukr_films_db.iloc[i, 1], ukr_films_db.iloc[i, 0], \
+                                              ukr_films_db.iloc[i, 0])
+                else:
+                    html = """<body>
+                                    <h4>{}</h4>
+                                    Year: {}<br>
+                                    Additional info: {}
+                                    <a href="https://www.google.com/search?q=%22{}%22"target="_blank">{}</a>
+                                    </body>
+                                    """
+                    html_format = html.format(ukr_films_db.iloc[i, 0], ukr_films_db.iloc[i, 1], ukr_films_db.iloc[i, 2], \
+                                              ukr_films_db.iloc[i, 0], ukr_films_db.iloc[i, 0])
+
+                iframe = folium.IFrame(html=html_format,
+                                       width=200,
+                                       height=100)
+                point = ukr_films_db.iloc[i, 4]
+                if point:
+                    lat = point[0]
+                    lon = point[1]
+                    ukr_fg.add_child(folium.Marker(location=[lat, lon],
+                                                   popup=folium.Popup(iframe),
+                                                   icon=folium.Icon(color="blue")))
+
+        return ukr_fg
+
+    check = False
+    try:
+        map = folium.Map(location=[latitude, longitude],
+                         zoom_start=3)
+        check = True
+    except:
+        print("Wrong coordinates!")
+    if check:
+        fg = nearest_films(data_base)
+        ukr_fg = ukr_films(full_data_base)
+
+        map.add_child(ukr_fg)
         map.add_child(fg)
         map.add_child(folium.LayerControl())
         map.save('Films_map.html')
 
 
 def main():
-    for i in range(1909, 3000):
+    for i in range(2000, 3000):
         pars_res = parsing([str(i), "80.2323", "23.67", "data/processed_locations_list(full)"])
         read_results = read_csv(pars_res[3])
         db = geolocation(read_results[0], pars_res[0], pars_res[1], pars_res[2], read_results[1])
-        #creating_map(db, pars_res[1], pars_res[2]) reminder
+        # creating_map(db, pars_res[1], pars_res[2], read_results[0]) reminder
         print(i)
-        with open("count.txt", encoding="utf-8", mode="a") as counter:
+        with open("count.txt", encoding="utf-8", mode="a") as counter:  # reminder
             counter.write("year processed: " + str(i) + ", total films: " + str(count[i]) + "\n")
 
 
